78702bc2a6fccf8e2eee44a724c084bf1b608d57
Murtada al Mousawy <info@murtada.nl>
2025-01-02 03:16:20 +0100

    Add Supabase types.

--- src/app/components/events.tsx
+++ src/app/components/events.tsx
@@ -1,22 +1,26 @@
 import { createClient } from '@/utils/supabase/server';
 import Image from 'next/image';
 import crypto from 'crypto';
+import { Database } from '../../../database.types';
+
+type ExtendedEvent = Database['public']['Tables']['events']['Row'] & {
+  attendees?: Database['public']['Tables']['events_rsvps']['Row'][];
+};
 
 export default async function Events() {
   const supabase = await createClient();
 
-  const { data: events } = await supabase.from("events").select();
+  const response = await supabase.from("events").select();
+  const events = response.data as ExtendedEvent[];
 
-  const promises = events?.map(async (event) => {
+  const promises = events.map(async (event) => {
     // Get the attendees for each event
     const { data: attendees } = await supabase.from("events_rsvps").select().eq("event_id", event.id);
 
-    event.attendees = attendees;
+    event.attendees = attendees!;
   });
 
   await Promise.all(promises!);
-
-  console.log(events);
 
   return (
     <section
@@ -31,7 +35,7 @@ export default async function Events() {
                 <h3 className="text-2xl font-bold">{event.title}</h3>
                 <hr />
                 <p className='mb-2'>{event.date}</p>
-                <p className='mb-2'>{event.location.replace(/\r\n/gm, ' • ')}</p>
+                <p className='mb-2'>{event.location?.replace(/\r\n/gm, ' • ')}</p>
                 <p className='whitespace-pre-line'>{event.description}</p>
                 <hr />
                 <div className='flex items-center gap-2'>
@@ -43,7 +47,7 @@ export default async function Events() {
                         width={32}
                         height={32}
                         className="z-10 -mr-2 size-8 rounded-full shadow"
-                        src={`https://gravatar.com/avatar/${crypto.createHash('md5').update(attendee.email).digest("hex")}?s=64`} alt="Gravatar"
+                        src={`https://gravatar.com/avatar/${crypto.createHash('md5').update(attendee.email || '').digest("hex")}?s=64`} alt="Gravatar"
                       />
                     ))}
                   </div>
@@ -55,7 +59,7 @@ export default async function Events() {
                 height={640}
                 alt='Event cover image'
                 className='h-full w-60 rounded-md object-cover'
-                src={event.cover_image}
+                src={event.cover_image!}
               />
             </div>
           ))}
