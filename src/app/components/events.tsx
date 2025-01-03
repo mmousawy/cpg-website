6656728cebb12a07fa7821c8b24109cbe54d98f1
Murtada al Mousawy <info@murtada.nl>
2025-01-03 02:58:42 +0100

    Refactor to get events at page level.

--- src/app/components/events.tsx
+++ src/app/components/events.tsx
@@ -1,32 +1,14 @@
-import { createClient } from '@/utils/supabase/server';
 import Image from 'next/image';
 import crypto from 'crypto';
-import { Database } from '../../../database.types';
+
+import { ExtendedEvent } from '../page';
 
 import CalendarSVG from 'public/icons/calendar2.svg';
 import LocationSVG from 'public/icons/location.svg';
 import TimeSVG from 'public/icons/time.svg';
 import SignupButton from './SignupButton';
 
-type ExtendedEvent = Database['public']['Tables']['events']['Row'] & {
-  attendees?: Database['public']['Tables']['events_rsvps']['Row'][];
-};
-
-export default async function Events() {
-  const supabase = await createClient();
-
-  const response = await supabase.from("events").select();
-  const events = response.data as ExtendedEvent[];
-
-  const promises = events.map(async (event) => {
-    // Get the attendees for each event
-    const { data: attendees } = await supabase.from("events_rsvps").select().eq("event_id", event.id);
-
-    event.attendees = attendees!;
-  });
-
-  await Promise.all(promises!);
-
+export default async function Events({ events }: { events: ExtendedEvent[] }) {
   return (
     <section
       className="flex justify-center bg-background px-6 pb-10 pt-8 text-foreground sm:p-12"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,38 @@
+import { createClient } from '@/utils/supabase/server';
+import { Database } from '../../database.types';
+
 import About from "./components/about";
 import Events from "./components/events";
 import Header from "./components/header";
 
-export default function Home() {
+export type ExtendedEvent = Database['public']['Tables']['events']['Row'] & {
+  attendees?: Database['public']['Tables']['events_rsvps']['Row'][];
+};
+
+export default async function Home() {
+  const supabase = await createClient();
+
+  const response = await supabase.from("events").select();
+  const events = response.data as ExtendedEvent[];
+
+  const promises = events.map(async (event) => {
+    // Get the attendees for each event
+    const { data: attendees } = await supabase.from("events_rsvps").select().eq("event_id", event.id);
+
+    event.attendees = attendees!;
+  });
+
+  await Promise.all(promises!);
+
   return (
     <div className="bg-background font-[family-name:var(--font-inter)]">
       <main>
         <Header />
-        <Events />
+        <Events events={events}/>
         <About />
       </main>
     </div>
   );
 }
+
+export const revalidate = 3600;
