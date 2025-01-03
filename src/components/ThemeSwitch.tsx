2a5f220ae585e7804dfe7c1aa84d388613c94383
Murtada al Mousawy <info@murtada.nl>
2025-01-03 19:02:18 +0100

    Make theme switch smarter.

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -1,42 +1,62 @@
 'use client';
 
 import { useTheme } from "next-themes";
-import { useEffect, useCallback } from "react";
+import { useEffect, useCallback, useState } from "react";
 import clsx from 'clsx';
 
 export default function ThemeSwitch() {
-  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme, systemTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true)
+  }, []);
 
   useEffect(() => {
     // If the theme is set to "system", we need to check the user's system preference
     if (theme === "system") {
-      if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      if (systemTheme === "dark") {
         setTheme("dark");
-      } else {
+      } else if (systemTheme === "light") {
         setTheme("light");
       }
     }
-  }, []);
+  }, [theme, systemTheme, setTheme]);
 
   const toggleTheme = useCallback(() => {
     setTheme(theme === "dark" ? "light" : "dark");
   }, [theme, setTheme]);
 
+  const themeButtonHandle = (
+    <span
+      className={clsx([
+        'block size-4 rounded-full bg-background-light shadow-md transition-transform dark:bg-[#ffffff60]',
+        (theme === 'light') && 'translate-x-6',
+      ])}
+    />
+  );
+
   return (
-    <div className="flex items-center justify-center gap-2 *:cursor-pointer">
-      <label htmlFor="themeButton" className="opacity-70">Switch theme</label>
+    <div
+      className="flex items-center justify-center gap-2"
+    >
+      <label htmlFor="themeButton" className="cursor-pointer opacity-70">Switch theme</label>
       <button
         id="themeButton"
         onClick={toggleTheme}
-        className="h-6 w-12 rounded-full bg-border-color p-1 text-foreground"
+        className="h-6 w-12 cursor-pointer rounded-full bg-border-color p-1 text-foreground "
         aria-label={`Toggle light or dark mode`}
       >
-        <span
-          className={clsx([
-            'block size-4 rounded-full bg-background-light shadow-md transition-transform dark:bg-[#ffffff60]',
-            theme === 'light' && 'translate-x-6',
-          ])}
-        />
+        {mounted
+          ? themeButtonHandle
+          : (
+            <span
+              className={clsx([
+                'block size-4 rounded-full bg-background-light shadow-md transition-transform dark:bg-[#ffffff60]',
+              ])}
+            />
+          )
+        }
       </button>
     </div>
   );
