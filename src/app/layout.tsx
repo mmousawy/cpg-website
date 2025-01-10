fde9ad236aec456b2f038a8a52ccfde62997d802
Murtada al Mousawy <info@murtada.nl>
2025-01-10 23:35:07 +0100

    Add Vercel speed insights.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Inter, Geist_Mono } from "next/font/google";
 
 import { ThemeProviderWrapper as ThemeProvider } from "@/app/providers/ThemeProvider";
@@ -44,6 +45,7 @@ export default function RootLayout({
           </ThemeProvider>
         </SupabaseProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
