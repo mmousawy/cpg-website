e8179ebddf7eb1f66fb852de3fef14e6ceb0ba1f
Murtada al Mousawy <hello@murtada.nl>
2026-01-18 22:22:28 +0100

    fix: Add svg.d.ts to TypeScript configuration for improved type support

---
+++ svg.d.ts
@@ -0,0 +1,32 @@
+declare module '*.svg' {
+  import React from 'react';
+  const SVG: React.FC<React.SVGProps<SVGSVGElement>>;
+  export default SVG;
+}
+
+declare module 'public/icons/*.svg' {
+  import React from 'react';
+  const SVG: React.FC<React.SVGProps<SVGSVGElement>>;
+  export default SVG;
+}
+
+declare module 'public/*.svg' {
+  import React from 'react';
+  const SVG: React.FC<React.SVGProps<SVGSVGElement>>;
+  export default SVG;
+}
+
+declare module 'public/*.png' {
+  const value: string;
+  export default value;
+}
+
+declare module 'public/*.jpg' {
+  const value: string;
+  export default value;
+}
+
+declare module 'public/*.jpeg' {
+  const value: string;
+  export default value;
+}
